401201394c0980ae14b7cf7d5d4c870149d955e5
David Lee <Dai180293@gmail.com>
2023-07-27 08:50:14 +0100

    challenge completed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/FEM-rest-api">
+        <BrowserRouter>
             <Routes>
                 <Route path="/*" element={<App />} />
             </Routes>
